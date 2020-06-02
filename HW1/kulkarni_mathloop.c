ff81108973fc148c1aeb9c1684328a5523b29e03
Ninad Kulkarni <ninadk03@gmail.com>
2020-06-01 17:31:57 -0700

    homework 1 completed files

--- HW1/kulkarni_mathloop.c
+++ HW1/kulkarni_mathloop.c
@@ -2,16 +2,18 @@
 void main()
 {
 	char ch;
-    int f, a = 10, b = 20;
+    double x, a = 10, b = 20;
+    int f;
 	int i = 0;
 	while(i < 5){
+        printf("Please enter an operator:  ");
 		scanf(" %c", &ch);
         printf("ch = %c\n", ch);
 	    switch(ch){
 		    case '+': f = a + b; printf("f = %d\n", f); break;
 		    case '-': f = a - b; printf("f = %d\n", f); break;
 		    case '*': f = a * b; printf("f = %d\n", f); break;
-		    case '/': f = a / b; printf("f = %d\n", f); break;
+		    case '/': x = a / b; printf("f = %f\n", x); break;
 		    default: printf("invalid operator\n"); break;
 	    }
 		i++;
