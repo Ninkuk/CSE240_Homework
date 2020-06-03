c9a4dd99c43f7bda7788ff0ceeff68aba354407a
ninkuk <ninadk03@gmail.com>
2020-06-02 20:14:54 -0700

    Comman line args exception handling implemented. Mini Games implementation started

--- HW2/kulkarni_ninad_hw2.c
+++ HW2/kulkarni_ninad_hw2.c
@@ -7,10 +7,15 @@ Description: TODO
 #include <stdio.h>
 
 // Forward Declaration Functions
+int commandLineArgsCheck(int);
 void printMenu();
+void miniGames(int, char**);
+void throwDown();
 
 int main(int argc, char** argv)
-{
+{	
+	if(commandLineArgsCheck(argc)) return -1;
+		
 	printMenu();
 	int selectedOption = 0;
 	int successCode = 0;
@@ -24,6 +29,19 @@ int main(int argc, char** argv)
 			printMenu();
 		}
 	} while(successCode == 0 || selectedOption > 2);
+
+	switch(selectedOption)
+	{
+		case 1:
+			miniGames(argc, argv);
+			break;
+		case 2:
+			//throwDown();
+			break;
+		case 0:
+			printf("Thank you for playing. Come back again!");
+			return 0;
+	}
 }
 
 void printMenu()
@@ -34,4 +52,36 @@ void printMenu()
 	printf("2 - Macros vs Functions Throw-down\n");
 	printf("0 - Exit\n");
 	printf("Choose an option: ");
+}
+
+void miniGames(int argc, char** argv)
+{	
+	char name[10];
+	if (argc > 0) {
+		
+	}	
+	//TODO create opponent
+	//generate random num between 0 and number of names in file
+	//read name from file IO
+}
+
+
+
+
+
+
+
+
+
+int commandLineArgsCheck(int argc)
+{
+	if (argc == 2) {
+		printf("Insufficient amount of arguments provided: %d\n", argc);
+		return 1;
+	} else if(argc > 3) {
+		printf("Too many arguments provided: %d\n", argc);
+		return 1;
+	}
+
+	return 0;
 }
